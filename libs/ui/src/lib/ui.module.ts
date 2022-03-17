d1ff3c7a88cbb01a9aa6f7b29330477398425b99
marininik <m.nikolli@lswr.it>
2022-03-17 12:23:43 +0100

    module and libraries

---
+++ libs/ui/src/lib/ui.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BannerComponent } from './banner/banner.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    BannerComponent
+  ]
+})
+
+export class UiModule {}
