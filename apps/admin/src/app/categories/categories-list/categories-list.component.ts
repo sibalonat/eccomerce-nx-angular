f8802def6ca97d5777bd78840a6e3a302ca9e741
marininik <m.nikolli@lswr.it>
2022-03-17 18:19:49 +0100

    .

--- apps/admin/src/app/categories/categories-list/categories-list.component.ts
+++ apps/admin/src/app/categories/categories-list/categories-list.component.ts
@@ -8,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesListComponent implements OnInit {
 
+  categories = [
+    {
+      id: 1,
+      name: "category-1",
+      icon: "icon-1"
+    },
+    {
+      id: 2,
+      name: "category-2",
+      icon: "icon-1"
+    },
+    {
+      id: 3,
+      name: "category-3",
+      icon: "icon-1"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
