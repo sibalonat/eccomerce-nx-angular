6ec1a43b44bbd655799683b4da612cb7cfffdc9a
marininik <m.nikolli@lswr.it>
2022-03-24 15:58:40 +0100

    products page in library

--- apps/artizanave/src/app/app.module.ts
+++ apps/artizanave/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 // import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { ProductListComponent } from './product-list/product-list.component';
+
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavComponent } from './shared/nav/nav.component';
@@ -19,8 +19,7 @@ import { AccordionModule } from 'primeng/accordion';
 
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'products', component: ProductListComponent }
+  { path: '', component: HomePageComponent }
 ];
 
 
@@ -29,7 +28,6 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     HomePageComponent,
-    ProductListComponent,
     HeaderComponent,
     FooterComponent,
     NavComponent],
--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -2,28 +2,40 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductSearchComponent } from './components/product-search/product-search.component';
 import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 import { ButtonModule } from 'primeng/button';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
+
+const routes: Routes = [
+  {
+    path: 'products',
+    component: ProductsListComponent
+  }
+];
+
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
-    ButtonModule
+    ButtonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [
     ProductSearchComponent,
     CategoriesBannerComponent,
     ProductItemComponent,
-    FeaturedProductsComponent
+    FeaturedProductsComponent,
+    ProductsListComponent
   ],
   exports: [
     ProductSearchComponent,
     CategoriesBannerComponent,
     ProductItemComponent,
-    FeaturedProductsComponent
+    FeaturedProductsComponent,
+    ProductsListComponent
   ]
 })
 
