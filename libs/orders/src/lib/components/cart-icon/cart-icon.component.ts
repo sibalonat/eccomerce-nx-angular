5a4323302dffafcd6ac2cad8c129bb80bc7dc3fb
marininik <m.nikolli@lswr.it>
2022-03-25 16:32:01 +0100

    cart

--- libs/orders/src/lib/components/cart-icon/cart-icon.component.ts
+++ libs/orders/src/lib/components/cart-icon/cart-icon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'orders-cart-icon',
@@ -7,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class CartIconComponent implements OnInit {
-
-  constructor() { }
+  cartCount?: any = 0;
+  constructor(
+    private cartSrv: CartService
+  ) { }
 
   ngOnInit(): void {
+    this.cartCount = this.cartSrv.getCart().items?.length;
+    console.log(this.cartCount);
   }
 
 }
--- libs/orders/src/lib/orders.module.ts
+++ libs/orders/src/lib/orders.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CartService } from './services/cart.service';
 import { CartIconComponent } from './components/cart-icon/cart-icon.component';
 
+import { BadgeModule } from 'primeng/badge';
+
 @NgModule({
   imports: [
     CommonModule,
+    BadgeModule
   ],
   providers: [],
   declarations: [
